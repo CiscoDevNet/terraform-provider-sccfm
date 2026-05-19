de938f24bf8382e523b523e1b75b0354895fee49
Isak Gilbert <isgilber@cisco.com>
2026-05-19 14:56:32 +0100

    LH-105100: Fix example params.

--- provider/examples/data-sources/msp/tenants/providers.tf
+++ provider/examples/data-sources/msp/tenants/providers.tf
@@ -7,7 +7,6 @@ terraform {
 }
 
 provider "sccfm" {
-  base_url = "https://staging.manage.security.cisco.com"
-  # base_url = "<https://us.manage.security.cisco.com|https://eu.manage.security.cisco.com|https://apj.manage.security.cisco.com|https://aus.manage.security.cisco.com|https://in.manage.security.cisco.com>"
+  base_url = "<https://us.manage.security.cisco.com|https://eu.manage.security.cisco.com|https://apj.manage.security.cisco.com|https://aus.manage.security.cisco.com|https://in.manage.security.cisco.com>"
   api_token = file("${path.module}/api_token.txt")
 }
